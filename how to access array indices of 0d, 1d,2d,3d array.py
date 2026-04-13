640170719211311e31d94f84179265d978b537ef
Md Zafir Tahmid Joarder <shomriddho8@gmail.com>
2026-04-13 22:19:55 +0600

    Add files via upload

---
+++ how to access array indices of 0d, 1d,2d,3d array.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array(25)
+b=np.array([[10,20,30]])
+c=np.array([[1,2,3], [4,5,6]])
+d=np.array([[1,3,5,7,9], [2,4,6,8,10], [5,10,15,20,25]])
+print("0d array:",a)
+print("1d array:",b[2:4])
+print("2d array:",c[:, 2])
+print("3d array",d[1, 1])
