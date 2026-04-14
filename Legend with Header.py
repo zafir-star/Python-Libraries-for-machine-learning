1de62e2574dc30b526f36ad5dd4cf231b28f04b6
Md Zafir Tahmid Joarder <shomriddho8@gmail.com>
2026-04-14 19:43:18 +0600

    Add files via upload

---
+++ Legend with Header.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+y = np.array([35, 25,25, 15])
+mylabels = ["Apples", "Bananas","Cherries","Dates"]
+plt.pie(y, labels = mylabels)
+plt.legend(title = "Four Fruits:")
+plt.show()
